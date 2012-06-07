554e733c1ba69a5a83fe47f96aa711e1407cbc56
Robert DeNicola <thedizzle23@gmail.com>
2012-06-07 13:57:06 -0400

    here is message

---
+++ UtilityPro/UtilityProTests/UtilityProTests.h
@@ -0,0 +1,13 @@
+//
+//  UtilityProTests.h
+//  UtilityProTests
+//
+//  Created by Robert DeNicola on 6/7/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UtilityProTests : SenTestCase
+
+@end
